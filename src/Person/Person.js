3411ebed36bf5be8cdc74ff4ac01c68fdde33f1f
iliyanBachiyski <iliyanbachiyski@gmail.com>
2019-03-13 12:44:44 +0200

    Added style in both ways: external with css file and inline with js object

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import "./Person.css";
 const person = props => {
   const title = "Hello, this is simple functional(stateless) component!";
+  const style = {
+    backgroundColor: "#cceeff",
+    border: "1px solid blue"
+  };
   return (
-    <div>
+    <div className="Person">
       <h3>{title}</h3>
       <h4>
         My name is {props.name}, and I am {props.age} years old!
@@ -13,10 +17,9 @@ const person = props => {
         onChange={event => props.changeName(event, props.name)}
         value={props.name}
       />
-      <button onClick={() => props.increaseAge(props.name)}>
+      <button style={style} onClick={() => props.increaseAge(props.name)}>
         Increase Age!
       </button>
-      <hr />
     </div>
   );
 };
