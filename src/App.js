857269aeabb55608374a34c95818fe13ff75921e
iliyanBachiyski <iliyanbachiyski@gmail.com>
2019-03-18 10:42:54 +0200

    Introduce conditionall rendering(inside and outside checking)

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ import "./App.css";
 
 class App extends Component {
   state = {
+    showPersons: false,
+    showCars: false,
     title: "Hello from App Component!",
-    persons: [{ name: "Iliyan", age: 24 }, { name: "Veronika", age: 23 }],
-    cars: [{ color: "Red", hp: 240 }, { color: "Grey", hp: 101 }]
+    persons: [
+      { name: "Iliyan", age: 24 },
+      { name: "Veronika", age: 23 },
+      { name: "Ivelin", age: 22 }
+    ],
+    cars: [
+      { color: "Red", hp: 240 },
+      { color: "Grey", hp: 101 },
+      { color: "Blue", hp: 150 }
+    ]
   };
 
   changeTitleHandler = () => {
@@ -35,28 +45,70 @@ class App extends Component {
     this.setState({ persons: newPersonsArray });
   };
 
+  tooglePersonHandler = () => {
+    this.setState({
+      showPersons: !this.state.showPersons
+    });
+  };
+
+  toogleCarsHandler = () => {
+    this.setState({
+      showCars: !this.state.showCars
+    });
+  };
+
   render() {
+    let cars = null;
+    if (this.state.showCars) {
+      cars = (
+        <div>
+          <Car color={this.state.cars[0].color} hp={this.state.cars[0].hp}>
+            Model: Ferari
+          </Car>
+          <Car color={this.state.cars[1].color} hp={this.state.cars[1].hp} />
+          <Car color={this.state.cars[2].color} hp={this.state.cars[2].hp} />
+        </div>
+      );
+    } else {
+      cars = (
+        <div>
+          <p>Press the button to Show/Hide cars!</p>
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>{this.state.title}</h1>
-        <button onClick={this.changeTitleHandler}>Change Title</button>
+        <button onClick={this.tooglePersonHandler}>Show/Hide Persons</button>
+        <button onClick={this.toogleCarsHandler}>Show/Hide Cars</button>
         <hr />
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          increaseAge={this.increasePersonAge}
-          changeName={this.changeNameHandler}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          increaseAge={this.increasePersonAge}
-          changeName={this.changeNameHandler}
-        />
-        <Car color={this.state.cars[0].color} hp={this.state.cars[0].hp}>
-          Model: Ferari
-        </Car>
-        <Car color={this.state.cars[1].color} hp={this.state.cars[1].hp} />
+        {this.state.showPersons ? (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+              increaseAge={this.increasePersonAge}
+              changeName={this.changeNameHandler}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              increaseAge={this.increasePersonAge}
+              changeName={this.changeNameHandler}
+            />
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+              increaseAge={this.increasePersonAge}
+              changeName={this.changeNameHandler}
+            />
+          </div>
+        ) : (
+          <div>
+            <p>Press the button to Show/Hide persons!</p>
+          </div>
+        )}
+        {cars}
         <Computer />
       </div>
     );
