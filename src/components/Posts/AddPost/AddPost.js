3824e3209cdb8f50c4598333dc87dc91086ff972
iliyanBachiyski <iliyanbachiyski@gmail.com>
2019-03-28 16:28:21 +0200

    Implemented Add post functionality with HTTP Post.

--- src/components/Posts/AddPost/AddPost.js
+++ src/components/Posts/AddPost/AddPost.js
@@ -1,36 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import PostInput from "./PostInput/PostInput";
+import axios from "axios";
 
-const addPost = props => (
-  <div className="card">
-    <h2>Add a Post</h2>
-    <PostInput
-      title="Title"
-      textChangeHandler={e => {
-        console.log(e.target.value);
-      }}
-    />
-    <PostInput
-      title="Content"
-      textChangeHandler={e => {
-        console.log(e.target.value);
-      }}
-    />
-    <PostInput
-      title="Author"
-      textChangeHandler={e => {
-        console.log(e.target.value);
-      }}
-    />
-    <button
-      style={{ backgroundColor: "green" }}
-      onClick={() => {
-        console.log("Post Adding...");
-      }}
-    >
-      Add Post
-    </button>
-  </div>
-);
+class AddPost extends Component {
+  state = {
+    postTitle: "",
+    postContent: "",
+    postAuthor: ""
+  };
 
-export default addPost;
+  postDataHandler = () => {
+    const post = {
+      title: this.state.postTitle,
+      body: this.state.postContent,
+      author: this.state.postAuthor
+    };
+    axios
+      .post("https://jsonplaceholder.typicode.com/posts", post)
+      .then(response => {
+        console.log(response);
+      });
+  };
+  render() {
+    return (
+      <div className="card">
+        <h2>Add a Post</h2>
+        <PostInput
+          title="Title"
+          textChangeHandler={e => {
+            this.setState({ postTitle: e.target.value });
+          }}
+        />
+        <PostInput
+          title="Content"
+          textChangeHandler={e => {
+            this.setState({ postContent: e.target.value });
+          }}
+        />
+        <PostInput
+          title="Author"
+          textChangeHandler={e => {
+            this.setState({ postAuthor: e.target.value });
+          }}
+        />
+        <button
+          style={{ backgroundColor: "green" }}
+          onClick={this.postDataHandler}
+        >
+          Add Post
+        </button>
+      </div>
+    );
+  }
+}
+
+export default AddPost;
