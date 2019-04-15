e342579a8a81e39b7ce783611cffe01b6fee451e
iliyanBachiyski <iliyanbachiyski@gmail.com>
2019-04-15 15:16:22 +0300

    Introduce action creators.

--- src/store/actions/actionConst.js
+++ src/store/actions/actionConst.js
@@ -6,3 +6,31 @@ export const DELETE_PERSON_ACTION = "DELETE_PERSON";
 export const INCREASE_RAM_ACTION = "INCREASE_RAM";
 export const RESET_RAM_ACTION = "RESET_RAM";
 export const SIMULATE_ERROR_ACTION = "SIMULATE_ERROR";
+
+export const tooglePersons = () => {
+  return { type: TOOGLE_PERSONS_ACTION };
+};
+
+export const increaseAge = personName => {
+  return { type: INCREASE_AGE_ACTION, payload: { personName } };
+};
+
+export const changePersonName = (event, oldPersonName) => {
+  return { type: CHANGE_PERSON_NAME_ACTION, payload: { event, oldPersonName } };
+};
+
+export const deletePersonName = personIdx => {
+  return { type: DELETE_PERSON_ACTION, payload: { personIdx } };
+};
+
+export const increaseRAM = () => {
+  return { type: INCREASE_RAM_ACTION };
+};
+
+export const resetRAM = () => {
+  return { type: RESET_RAM_ACTION };
+};
+
+export const simulateError = () => {
+  return { type: SIMULATE_ERROR_ACTION };
+};
--- src/store/actions/personActions/mapDispatchToProps.js
+++ src/store/actions/personActions/mapDispatchToProps.js
@@ -1,22 +1,17 @@
 import {
-  INCREASE_AGE_ACTION,
-  TOOGLE_PERSONS_ACTION,
-  CHANGE_PERSON_NAME_ACTION,
-  DELETE_PERSON_ACTION
+  increaseAge,
+  tooglePersons,
+  changePersonName,
+  deletePersonName
 } from "../actionConst";
 
 const mapDispatchToProps = dispatch => {
   return {
-    onTooglePersons: () => dispatch({ type: TOOGLE_PERSONS_ACTION }),
-    onIncreasePersonAge: personName =>
-      dispatch({ type: INCREASE_AGE_ACTION, payload: { personName } }),
+    onTooglePersons: () => dispatch(tooglePersons()),
+    onIncreasePersonAge: personName => dispatch(increaseAge(personName)),
     onChangeName: (event, oldPersonName) =>
-      dispatch({
-        type: CHANGE_PERSON_NAME_ACTION,
-        payload: { event, oldPersonName }
-      }),
-    onDeletePerson: personIdx =>
-      dispatch({ type: DELETE_PERSON_ACTION, payload: { personIdx } })
+      dispatch(changePersonName(event, oldPersonName)),
+    onDeletePerson: personIdx => dispatch(deletePersonName(personIdx))
   };
 };
 
